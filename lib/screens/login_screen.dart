0fbd054ff9e27a415244543528b83a18bc667a1c
Angel Godinez <angel.godinez@resser.com>
2020-06-25 23:10:42 -0500

    login merge with develop

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:dulces_client_a/screens/register_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:dulces_client_a/service/auth.dart';
 
 class LoginScreen extends StatefulWidget {
   static String id = 'login_screen';
@@ -15,6 +16,62 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool showSpinner = false;
+  final txtEmailController = TextEditingController();
+  final txtPassController = TextEditingController();
+  
+  void loginAction(BuildContext context) async {
+
+    String email = txtEmailController.text;
+    String password = txtPassController.text;
+
+    if(password.length < 6){
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+            return AlertDialog(
+                title: Text('La contraseña debe tener al menos 6 caracteres'),
+                elevation: 24,
+                actions: <Widget>[
+                  FlatButton( 
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }, 
+                  )
+                ],
+              );
+            }
+        );
+    }else{
+      Auth auth = Auth();
+      String uid = await auth.signIn(email, password);
+      if(uid != "error"){
+        setState(() {
+          Navigator.pushNamed(context, ListViewProducts.id);
+        });
+      }else{
+        showDialog(
+        context: context,
+        builder: (BuildContext context) {
+            return AlertDialog(
+                title: Text('Ocurrió un error, por favor vuelve a intentar'),
+                elevation: 24,
+                actions: <Widget>[
+                  FlatButton( 
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }, 
+                  )
+                ],
+              );
+            }
+        );
+      }
+      
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,7 +1,9 @@
 import 'package:dulces_client_a/components/rounded_button.dart';
+import 'package:dulces_client_a/screens/login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
+import 'package:dulces_client_a/screens/listview_product.dart';
+import 'package:dulces_client_a/service/auth.dart';
 
 class RegisterScreen extends StatefulWidget {
   static String id = 'register_screen';
@@ -10,6 +12,65 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+  final txtNameController = TextEditingController();
+  final txtEmailController = TextEditingController();
+  final txtPassController = TextEditingController();
+
+  void registerUser() async {
+    String name = txtNameController.text;
+    String email = txtEmailController.text;
+    String password = txtPassController.text;
+    
+    if(password.length < 6){
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+            return AlertDialog(
+                title: Text('La contraseña debe tener al menos 6 caracteres'),
+                elevation: 24,
+                actions: <Widget>[
+                  FlatButton( 
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }, 
+                  )
+                ],
+              );
+            }
+        );
+    }else{
+      Auth auth = Auth();
+      String uid = await auth.signUp(email, password);
+      if(uid != "error"){
+        setState(() {
+          Navigator.pushNamed(context, ListViewProducts.id);
+        });
+      }else{
+        showDialog(
+        context: context,
+        builder: (BuildContext context) {
+            return AlertDialog(
+                title: Text('Ocurrió un error, por favor vuelve a intentar'),
+                elevation: 24,
+                actions: <Widget>[
+                  FlatButton( 
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }, 
+                  )
+                ],
+              );
+            }
+        );
+      }
+    }
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,21 +84,69 @@ class _RegisterScreenState extends State<RegisterScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text('Nombre'),
-                  ],
+                Text(
+                  'Crea tu usuario!',
+                  style: 
+                    TextStyle(color: Colors.white, fontSize: 24)
                 ),
+                SizedBox(
+                  height: 30.0,
+                ),
+
+                Text(
+                  'Ingresa tu nombre y apellido'
+                ),
+
+                TextField(
+                  controller: txtNameController,
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  maxLength: 40,
+                  maxLengthEnforced: true,
+                  style: TextStyle(
+                    color: Colors.white70,
+                  ),
+                  decoration: InputDecoration(
+                    hintText: 'Nombre y apellido',
+                    hintStyle: TextStyle(
+                      color: Colors.white70,
+                    ),
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xFFECA703),
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber, width: 2.0),
+                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 30.0,
+                ),
+
                 Column(
                   children: <Widget>[
                     TextField(
                       keyboardType: TextInputType.emailAddress,
+                      controller: txtEmailController,
                       textAlign: TextAlign.center,
+                      maxLength: 50,
+                      maxLengthEnforced: true,
                       style: TextStyle(
                         color: Colors.white70,
                       ),
                       decoration: InputDecoration(
-                        hintText: 'Ingresa tu Contrasena',
+                        hintText: 'Correo electronico',
                         hintStyle: TextStyle(
                           color: Colors.white70,
                         ),
@@ -52,6 +161,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
             TextField(
               keyboardType: TextInputType.emailAddress,
+              controller: txtPassController,
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.white70,
@@ -118,9 +228,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
               labelText: 'Registrarse',
               color: Colors.amber,
               onPressed: () {
-                setState(() {
-                  Navigator.pushNamed(context, LoginScreen.id);
-                });
+                registerUser();
               },
             ),
           ],
