5135dafc3c0e2af1a55974b26f404ead4a71b6e0
Absa Garcia <absalonsubzero@gmail.com>
2020-07-04 13:37:30 -0500

    absa cambios

--- lib/products/listview_product.dart
+++ lib/products/listview_product.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:dulces_client_a/navigation_bloc/navigation.dart';
@@ -10,9 +9,9 @@ import '../windows/product_information.dart';
 import '../products/product_screen.dart';
 import '../theme.dart' as Theme;
 
-
-class ListViewProducts extends StatefulWidget with NavigationStates{
+class ListViewProducts extends StatefulWidget with NavigationStates {
   @override
+  static String id = 'listview_products';
   _ListViewProductsState createState() => _ListViewProductsState();
 }
 
@@ -33,7 +32,6 @@ class _ListViewProductsState extends State<ListViewProducts> {
         productReference.onChildAdded.listen(_onProductAdded);
     _onProductChangeSubscription =
         productReference.onChildChanged.listen(_onProductUpdate);
-   
   }
 
   @override
@@ -41,112 +39,149 @@ class _ListViewProductsState extends State<ListViewProducts> {
     super.dispose();
     _onProductAddedSubscription.cancel();
     _onProductChangeSubscription.cancel();
-
   }
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child:  ListView.builder(
+        child: ListView.builder(
           padding: EdgeInsets.only(top: 12.0),
           itemCount: items.length,
           itemBuilder: (context, position) {
             return Container(
-              child: buildListTile(position),    
+              child: buildListTile(position),
             );
           },
         ),
-      ),  
+      ),
     );
   }
 
   Widget buildListTile(int position) {
     if (items[position].stock != null) {
-    return new Container(
-      height: 120.0,
-      margin: const EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 10.0),
-      decoration: new BoxDecoration(
-        borderRadius: new BorderRadius.all(new Radius.circular(30.0)),
-        boxShadow: [ BoxShadow( color: Theme.CompanyColors.black[400], blurRadius: 3.0, spreadRadius: 1.0, offset: Offset(0.0, 3.0)) ],
-          gradient: new LinearGradient(colors: [Theme.CompanyColors.black[50], Theme.CompanyColors.black[500]],
-              begin: Alignment.centerLeft, end: Alignment.centerRight, tileMode: TileMode.clamp)
+      return new Container(
+        height: 120.0,
+        margin: const EdgeInsets.only(
+            left: 10.0, right: 10.0, top: 5.0, bottom: 10.0),
+        decoration: new BoxDecoration(
+            borderRadius: new BorderRadius.all(new Radius.circular(30.0)),
+            boxShadow: [
+              BoxShadow(
+                  color: Theme.CompanyColors.black[400],
+                  blurRadius: 3.0,
+                  spreadRadius: 1.0,
+                  offset: Offset(0.0, 3.0))
+            ],
+            gradient: new LinearGradient(
+                colors: [
+                  Theme.CompanyColors.black[50],
+                  Theme.CompanyColors.black[500]
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                tileMode: TileMode.clamp)),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Padding(
+              padding: new EdgeInsets.only(left: 15.0, right: 10.0, top: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.asset('images/kitkat.png',
+                    width: 90.0, height: 90.0, fit: BoxFit.fill),
               ),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Padding(
-            padding: new EdgeInsets.only(left: 15.0, right: 10.0, top: 15),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-                child: Image.asset('images/kitkat.png', width: 90.0, height: 90.0, fit: BoxFit.fill),
-              ),
-            //CircleAvatar(radius: 35.0, backgroundImage: AssetImage('images/person.png'),)
-          ),
-          new Expanded(child: new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new SizedBox(height: 30.0,),
-              new Text('${items[position].name}', style: new TextStyle(fontSize: 18.0, color: Colors.white70, fontWeight: FontWeight.bold),),
-              new SizedBox(height: 8.0,),
-              new Text('${items[position].description}', style: new TextStyle(fontSize: 14.0, color: Colors.white70),),
-              new SizedBox(height: 10.0,),
-            ],),),
-          new Padding(padding: new EdgeInsets.only(left: 10.0, right: 10.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                FlatButton(
-                  onPressed: () {
-                    setState(() {
-                      _createOrder( context, items[position]);
-                    });
-                  },
-                  child: Icon(
-                    FontAwesomeIcons.plus,
-                    color: Theme.CompanyColors.black[500],
+              //CircleAvatar(radius: 35.0, backgroundImage: AssetImage('images/person.png'),)
+            ),
+            new Expanded(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new SizedBox(
+                    height: 30.0,
                   ),
-                  shape: CircleBorder(),
-                  color: Theme.CompanyColors.black[200],
-                ),
-              ],),),
-        ],
+                  new Text(
+                    '${items[position].name}',
+                    style: new TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.white70,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  new SizedBox(
+                    height: 8.0,
+                  ),
+                  new Text(
+                    '${items[position].description}',
+                    style: new TextStyle(fontSize: 14.0, color: Colors.white70),
+                  ),
+                  new SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
+            new Padding(
+              padding: new EdgeInsets.only(left: 10.0, right: 10.0),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        _createOrder(context, items[position]);
+                      });
+                    },
+                    child: Icon(
+                      FontAwesomeIcons.plus,
+                      color: Theme.CompanyColors.black[500],
+                    ),
+                    shape: CircleBorder(),
+                    color: Theme.CompanyColors.black[200],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       );
     }
   }
 
-void _createOrder(BuildContext context, Product product)async{
-  await Navigator.push(context, 
-   MaterialPageRoute(builder:(context)=> ProductInformation(product)),
-   );
-}
+  void _createOrder(BuildContext context, Product product) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ProductInformation(product)),
+    );
+  }
 
-void _onProductAdded(Event event) {
-  setState(() {
-    items.add(new Product.fromSnapShot(event.snapshot));
-  });
-}
+  void _onProductAdded(Event event) {
+    setState(() {
+      items.add(new Product.fromSnapShot(event.snapshot));
+    });
+  }
 
-void _onProductUpdate(Event event) {
-  var oldProductValue = items.singleWhere((product) =>  product.id == event.snapshot.key);
-  setState(() {
-    items[items.indexOf(oldProductValue)] = new Product.fromSnapShot(event.snapshot);
-  });
-}
+  void _onProductUpdate(Event event) {
+    var oldProductValue =
+        items.singleWhere((product) => product.id == event.snapshot.key);
+    setState(() {
+      items[items.indexOf(oldProductValue)] =
+          new Product.fromSnapShot(event.snapshot);
+    });
+  }
 
-void _navigateToProductInformation(BuildContext context, Product product)async{
-   await Navigator.push(context, 
-   MaterialPageRoute(builder:(context)=> ProductScreen(product)),
-   );
-}
+  void _navigateToProductInformation(
+      BuildContext context, Product product) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ProductScreen(product)),
+    );
+  }
 
-void _navigateToProduct(BuildContext context, Product product)async{
-   await Navigator.push(context, 
-   MaterialPageRoute(builder:(context)=> ProductInformation(product)),
-   );
+  void _navigateToProduct(BuildContext context, Product product) async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ProductInformation(product)),
+    );
+  }
 }
-
-}
-
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dulces_client_a/products/listview_product.dart';
+import 'package:dulces_client_a/service/auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
@@ -5,13 +7,65 @@ import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:dulces_client_a/navigation_bloc/navigation.dart';
 import '../components/rounded_button.dart';
 
-class LoginScreen extends StatefulWidget with NavigationStates{
+class LoginScreen extends StatefulWidget with NavigationStates {
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
   bool showSpinner = false;
+  final txtEmailController = TextEditingController();
+  final txtPassController = TextEditingController();
+  String email;
+  String password;
+
+  void loginAction(BuildContext context) async {
+    if (password.length <= 6) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('La contraseña debe tener al menos 6 caracteres'),
+              elevation: 24,
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    }
+    if (password.length > 6) {
+      Auth auth = Auth();
+      String uid = await auth.signIn(email, password);
+      if (uid != "error") {
+        setState(() {
+          BlocProvider.of<NavigationBloc>(context)
+              .add(NavigationEvents.PedidosEvent);
+        });
+      } else {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Ocurrió un error, por favor vuelve a intentar'),
+                elevation: 24,
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('OK'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              );
+            });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +91,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.white70,
+                  color: Colors.black,
                 ),
+                onChanged: (value) {
+                  email = value;
+                },
                 decoration: InputDecoration(
                   hintText: 'Ingresa tu Correo',
                   hintStyle: TextStyle(
-                    color: Colors.white70,
+                    color: Colors.black45,
                   ),
                   contentPadding:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
@@ -69,12 +126,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 keyboardType: TextInputType.emailAddress,
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  color: Colors.white70,
+                  color: Colors.black,
                 ),
+                onChanged: (value) {
+                  password = value;
+                },
                 decoration: InputDecoration(
                   hintText: 'Ingresa tu Contrasena',
                   hintStyle: TextStyle(
-                    color: Colors.white70,
+                    color: Colors.black45,
                   ),
                   contentPadding:
                       EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
@@ -101,7 +161,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 labelText: 'Ingresar',
                 color: Colors.amber,
                 onPressed: () {
-                  BlocProvider.of<NavigationBloc>(context).add(NavigationEvents.PedidosEvent);
+//                  BlocProvider.of<NavigationBloc>(context)
+//                      .add(NavigationEvents.PedidosEvent);
+
+                  loginAction(context);
                 },
               ),
               SizedBox(
@@ -109,8 +172,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               FlatButton(
                   textColor: Colors.white,
-                  onPressed: () {
-                  },
+                  onPressed: () {},
                   child: Text(
                     'Registrate Aqui',
                     style: TextStyle(
